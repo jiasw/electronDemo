cd3a839e55e0c0b3b9565a440ce78286b24045c6
jiasw <jswjia@163.com>
2025-07-26 16:46:02 +0800

    初始化

---
+++ preload.js
@@ -0,0 +1,9 @@
+const { contextBridge } = require('electron')
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  // 除函数之外，我们也可以暴露变量
+  version: "1.0.0"
+})
